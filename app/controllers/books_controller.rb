c9d245a3e12738770eec96eaa260e201e9ec7727
Eoin Gorman <eoingorman@live.ie>
2022-06-29 10:36:45 +0200

    add book and review details to show page, fix routing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
   end
 
   def show
-
+    @book = Book.find(params[:id])
+    @reviews = @book.reviews
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "books#index"
-  resources :book
+  resources :books
 end
